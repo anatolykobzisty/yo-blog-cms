3fd288f3060c4d94a78dc113bc2dd439b9d9fc0f
Anatoly Kobzisty <anatolykobzisty@gmail.com>
2021-08-05 21:49:02 +0300

    add post schema

---
+++ schemas/post.js
@@ -0,0 +1,36 @@
+export default {
+  name: 'post',
+  title: 'Статьи',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Название',
+      type: 'string',
+      description: 'Название статьи',
+    },
+    {
+      name: 'slug',
+      title: 'Слаг',
+      type: 'slug',
+      options: {
+        source: 'title',
+        maxLength: 100,
+      },
+    },
+    {
+      name: 'image',
+      title: 'Фото',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: 'body',
+      title: 'Текст',
+      type: 'text',
+      description: 'Текст статьи',
+    },
+  ],
+};
---
+++ schemas/schema.js
@@ -0,0 +1,14 @@
+/* eslint-disable import/no-unresolved */
+// 1. Импортируем schema creator
+import createSchema from 'part:@sanity/base/schema-creator';
+// 2. Импортируем типы schema-type
+import schemaTypes from 'all:part:@sanity/base/schema-type';
+// 3. Импортируем "схемы" наших данных
+import post from './post';
+
+export default createSchema({
+  // 4. Присваиваем название "схеме"
+  name: 'default',
+  // 5. Добавляем "схемы" наших данных в sanity
+  types: schemaTypes.concat([post]),
+});
